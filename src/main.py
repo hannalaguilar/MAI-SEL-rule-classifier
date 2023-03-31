1697916cbdbb36d45854f8c33be50990c1d8889b
hannalaguilar <hanna.laguilar@gmail.com>
2023-03-31 04:04:11 +0200

    main.py ta wonito

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+from pathlib import Path
+
 from src.cn2 import run_cn2
 
+folder_path = Path(__file__).parent.parent
 
 if __name__ == "__main__":
-    DATA_PATH = '../data/titanic.csv'
-    RULE_LIST = run_cn2(DATA_PATH, verbose=True)
+    DATASETS = ['titanic']
+    for dataset in DATASETS:
+        print('{:-^85}'.format(dataset.upper()))
+        data_path = folder_path / 'data' / f'{dataset}.csv'
+        RULE_LIST = run_cn2(data_path, verbose=True)
+        print('')
